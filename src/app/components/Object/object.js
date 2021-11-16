cdc4cdba789d0a80bd0c0ff120123565ee3697c1
Prithwiraj Samanta <prskid1000@gmail.com>
2021-11-16 17:16:56 +0530

    update

---
+++ src/app/components/Object/object.js
@@ -0,0 +1,11 @@
+import { React, useState, useEffect} from "react";
+import actions from '../../redux/actions';
+import { useStore } from 'react-redux';
+import './object.css';
+
+function Object() {
+    return (
+        <></>
+    );
+}
+export default Object;
