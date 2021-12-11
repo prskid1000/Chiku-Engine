5055a07f6cce5a4e85aa8c7d9adc670145c70ca8
Prithwiraj Samanta <prskid1000@gmail.com>
2021-12-11 21:43:52 +0530

    update

--- With Web-Worker/public/worker/worker.js
+++
@@ -1,6 +0,0 @@
-importScripts("object.js");
-self.onmessage = (message) => {
-    var simulate = new Function("message", message.data.statement.substring(11))
-    postMessage(simulate(message.data.args))
-    //postMessage(eval(message.data.statement)(message.data.args))
-}
--- With Web-Worker/src/processDOM.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    processDOM: (grid) => {
-        Object.keys(grid).map((key) => {
-            var cell = document.getElementById(key)
-            cell.style.backgroundColor = grid[key].color
-        })
-    }
-}
